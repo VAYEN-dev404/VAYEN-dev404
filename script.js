0e5139d36bfda344478f5771b749f0b75335744d
VAYEN-dev404 <jakethesnake2008@myyahoo.com>
2025-12-12 17:22:28 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function createSnowflake() {
+  const snowflake = document.createElement("div");
+  snowflake.classList.add("snowflake");
+  snowflake.textContent = "❄";
+  snowflake.style.left = Math.random() * 100 + "vw";
+  snowflake.style.animationDuration = 4 + Math.random() * 6 + "s";
+  snowflake.style.opacity = Math.random();
+
+  document.getElementById("snow").appendChild(snowflake);
+
+  setTimeout(() => {
+    snowflake.remove();
+  }, 10000);
+}
+
+setInterval(createSnowflake, 150);
