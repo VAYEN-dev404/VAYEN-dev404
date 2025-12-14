94209a181f59e734d771fc462d34fe6dde2e374c
VAYEN-dev404 <jakethesnake2008@myyahoo.com>
2025-12-13 21:28:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function createSnowflake() {
   snowflake.textContent = "❄";
 
   snowflake.style.left = Math.random() * 100 + "vw";
-  snowflake.style.fontSize = (10 + Math.random() * 18) + "px";
+  snowflake.style.fontSize = (12 + Math.random() * 18) + "px";
   snowflake.style.opacity = Math.random();
-  snowflake.style.animationDuration = (4 + Math.random() * 5) + "s";
+  snowflake.style.animationDuration = (2 + Math.random() * 3) + "s";
 
-  // 35% chance to sparkle
-  if (Math.random() < 0.35) {
+  // 40% chance to sparkle
+  if (Math.random() < 0.4) {
     snowflake.classList.add("sparkle");
   }
 
@@ -18,5 +18,5 @@ function createSnowflake() {
   setTimeout(() => snowflake.remove(), 8000);
 }
 
+// Snowflake spawn interval
 setInterval(createSnowflake, 90);
-
